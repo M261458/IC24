21b1ba05c7987aeac0cddb482ccb49cfff48e5be
Daniel Prather <m265106@usna.edu>
2024-02-27 13:59:05 -0500

    Dan added .csv file with stdev differences for all food groups

--- Z-score_by_mean.py
+++ Z-score_by_mean.py
@@ -18,3 +18,31 @@ for ind in stdevs_df.index: # calculates stdev difference for every row of df.
     stdev_list.append(abs((stdevs_df['FF Mean per 100g'][ind]-stdevs_df['SR Mean per 100g'][ind])/stdevs_df['stdev'][ind]))
 stdevs_df['stdev_difference'] = stdev_list # Adds stdev differences to df.
 print(stdevs_df) # 'stdevs_df' has Food description, both means, stdevs, and stdev differences.
+
+
+# for ind in stdevs_df.index:
+#     if stdevs_df['SR Food description'][ind] == 'Hummus, commercial':
+#         print(stdevs_df['Nutrient_id'][ind],stdevs_df['stdev_difference'][ind])
+
+
+# Find average STDEV error over each food group.
+mean0 = stdevs_df.groupby('SR Food description',as_index=False)[['stdev_difference']].mean()
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# DO NOT RUN THESE LINES AGAIN!!!!!!! THE FILE ALREADY EXISTS!!!!!!!!!!!!!!!!!!!!!!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# print(mean0)
+# mean_tup_list = []
+# for ind in mean0.index:
+#     mean_tup_list.append((mean0['SR Food description'][ind],mean0['stdev_difference'][ind]))
+
+# with open('food_stdev_diff.csv', 'w') as file:
+#     file.write('name,avg_stdev_diff\n')
+#     for tupl in mean_tup_list:
+#         file.write(f'"{tupl[0]}",{tupl[1]}\n')
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
--- example.py
+++ example.py
@@ -6,9 +6,22 @@ stdevs = stdevs.rename(columns={"mean2": "stdev"})
 
 
 df = df.merge(stdevs,on='name')
-print(df)
+
+
+
 stdev_list = []
 for ind in df.index:
     stdev_list.append(abs((df['mean2'][ind]-df['mean1'][ind])/df['stdev'][ind]))
 df['stdev_difference'] = stdev_list
-print(df)
+
+
+# Find average STDEV error over each food group.
+mean0 = df.groupby('name',as_index=False)[['stdev_difference']].mean()
+mean_tup_list = []
+for ind in mean0.index:
+    mean_tup_list.append((mean0['name'][ind],mean0['stdev_difference'][ind]))
+
+with open('mean_diff.csv', 'w') as file:
+    file.write('name,avg_stdev_diff\n')
+    for tupl in mean_tup_list:
+        file.write(f'{tupl[0]},{tupl[1]}\n')
