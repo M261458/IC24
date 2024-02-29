d6eea9ca43b303bfab9badd772320a3814231b9b
jillianbrodeur <m260684@usna.edu>
2024-02-29 08:29:53 -0500

    jill added something for once

--- jillsevaluation.py
+++ jillsevaluation.py
@@ -12,5 +12,12 @@ avg_stdev = food_stdev.sort_values('avg_stdev_diff', ascending = False)
 above3 = avg_stdev[ avg_stdev['avg_stdev_diff'] > 0.3 ]
 
 top10 = pd.read_csv('jillcsv.csv')
-fig = px.bar(top10, x = 'Name of Food', y = 'Average Standard Deviation Difference')
+namelist = []
+valuelist =[]
+for i,row in top10.iterrows():
+    namelist.append(top10['Name of Food'][i])
+    valuelist.append(top10['Average Standard Deviation Difference'][i])
+print(namelist)
+print(valuelist)
+fig = px.bar(top10, x = 'Name of Food', y = 'Average Standard Deviation Difference', color = 'Name of Food')
 fig.show()
